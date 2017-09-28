9a3a70b66c2d14f0463cbb6f44b1f282d3f10673
arashkashi@gmail.com <arashkashi@gmail.com>
2017-09-28 11:41:42 +0200

    added data provider
    data provider is a small wrapper to facilitate use of core data.

---
+++ JodelChallenge/DataProvider/FetchResultsControllerWithCollectionView.swift
@@ -0,0 +1,133 @@
+//
+//  FetchResultsControllerWithCollectionView.swift
+//  CoreStore
+//
+//  Created by Arash Kashi on 2/23/17.
+//  Copyright © 2017 Arash K. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+
+
+class FetchResultsControllerWithCollectionView: NSObject, NSFetchedResultsControllerDelegate {
+    
+    var collectionView: UICollectionView!
+    var fetchResultController: NSFetchedResultsController<NSManagedObject>
+    
+    init(fetchResultController: NSFetchedResultsController<NSManagedObject>
+        , collectionView: UICollectionView) {
+        
+        self.collectionView = collectionView
+        self.fetchResultController = fetchResultController
+        
+        super.init()
+        fetchResultController.delegate = self
+    }
+    
+    var blockOperations: [BlockOperation] = []
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        if type == NSFetchedResultsChangeType.insert {
+            print("Insert Object: \(String(describing: newIndexPath))")
+            
+            blockOperations.append(
+                BlockOperation(block: { [weak self] in
+                    if let this = self {
+                        this.collectionView!.insertItems(at: [newIndexPath!])
+                    }
+                })
+            )
+        }
+        else if type == NSFetchedResultsChangeType.update {
+            print("Update  Object: \(String(describing: indexPath))")
+            blockOperations.append(
+                BlockOperation(block: { [weak self] in
+                    if let this = self {
+                        this.collectionView!.reloadItems(at: [indexPath!])
+                    }
+                })
+            )
+        }
+        else if type == NSFetchedResultsChangeType.move {
+            print("Move) Object: \(String(describing: indexPath))")
+            
+            blockOperations.append(
+                BlockOperation(block: { [weak self] in
+                    if let this = self {
+                        this.collectionView!.moveItem(at: indexPath!, to: newIndexPath!)
+                    }
+                })
+            )
+        }
+        else if type == NSFetchedResultsChangeType.delete {
+            print("Delete) Object: \(String(describing: indexPath))")
+            
+            blockOperations.append(
+                BlockOperation(block: { [weak self] in
+                    if let this = self {
+                        this.collectionView!.deleteItems(at: [indexPath!])
+                    }
+                })
+            )
+        }
+    }
+    
+    public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        
+        
+        if type == NSFetchedResultsChangeType.insert {
+            print("Insert Section: \(sectionIndex)")
+            
+            blockOperations.append(
+                BlockOperation(block: { [weak self] in
+                    if let this = self {
+                        this.collectionView!.insertSections(NSIndexSet(index: sectionIndex) as IndexSet)
+                    }
+                })
+            )
+        }
+        else if type == NSFetchedResultsChangeType.update {
+            print("Update Section: \(sectionIndex)")
+            blockOperations.append(
+                BlockOperation(block: { [weak self] in
+                    if let this = self {
+                        this.collectionView!.reloadSections(NSIndexSet(index: sectionIndex) as IndexSet)
+                    }
+                })
+            )
+        }
+        else if type == NSFetchedResultsChangeType.delete {
+            print("Delete Section: \(sectionIndex)")
+            
+            blockOperations.append(
+                BlockOperation(block: { [weak self] in
+                    if let this = self {
+                        this.collectionView!.deleteSections(NSIndexSet(index: sectionIndex) as IndexSet)
+                    }
+                })
+            )
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        collectionView!.performBatchUpdates({ () -> Void in
+            for operation: BlockOperation in self.blockOperations {
+                operation.start()
+            }
+        }, completion: { (finished) -> Void in
+            self.blockOperations.removeAll(keepingCapacity: false)
+        })
+    }
+    
+    deinit {
+        for operation: BlockOperation in blockOperations {
+            operation.cancel()
+        }
+        
+        blockOperations.removeAll(keepingCapacity: false)
+    }
+}
---
+++ JodelChallenge/DataProvider/Utils.swift
@@ -0,0 +1,29 @@
+//
+//  Utils.swift
+//  CoreStore
+//
+//  Created by Arash Kashi on 12/13/16.
+//  Copyright © 2016 Arash K. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+
+func isRunningTests() -> Bool {
+    
+    return ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
+}
+
+
+extension NSLock {
+    
+    func withCriticalScope<T>(_ block: () -> T) -> T {
+        lock()
+        let value = block()
+        unlock()
+        return value
+    }
+}
+
